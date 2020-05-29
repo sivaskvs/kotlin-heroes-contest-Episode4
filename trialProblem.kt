ad4d0c2fe84a19bb59b87aa64b9e0cb73e4b0ce7
Siva Subramanian <sivasubramanian96@gmail.com>
2020-05-29 10:18:32 +0100

    Create trialProblem.kt

---
+++ trialProblem.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var cases = readLine()!!.toInt()
+    while(cases > 0){
+        val (a, b) = readLine()!!.split(' ')
+        println(a.toInt() + a.toInt())
+        cases -= 1
+    }
+}
